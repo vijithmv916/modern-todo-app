6fb1b6931f06e36ce64f771d0cd9c58c0661b860
vijithmv916 <miniboyviju916@gmail.com>
2022-07-16 15:33:16 +0530

    half completed

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,61 @@
-<script setup>
-</script>
-
 <template>
-<h1>hello</h1>
+  <div
+    class="min-h-screen bg-[#fefefe] w-full flex items-center justify-center"
+  >
+    <!-- container -->
+    <div
+      class="flex bg-[#f5f9f9] p-5 flex-col justify-center rounded-[30px] w-[680px]"
+    >
+      <!--col1 todo and badge -->
+      <div class="flex items-center pt-6 justify-between">
+        <h1 class="flex text-2xl font-medium text-gray-800">To do</h1>
+        <span
+          class="flex bg-[#e8f1f0] text-[#279682] text-lg p-1 rounded-xl px-3"
+          >2</span
+        >
+      </div>
+      <!-- col2 button -->
+      <div class="flex w-full mx-auto py-4">
+        <button
+          class="w-full text-4xl text-[#279682] h-[4rem] rounded-2xl bg-[#e7f0ef] hover:bg-[#cad4d4]"
+          @click="open = true"
+        >
+          +
+        </button>
+      </div>
+      <!-- col3 list -->
+      <div>
+        <ul v-for="item in myList" class="my-3">
+          <div
+            class="flex items-center p-2 border rounded-xl text-xl cursor-pointer hover:bg-white/50"
+            @click="item.checked = !item.checked"
+          >
+            <input type="checkbox" :checked=item.checked name="" id="" class="w-5 h-5 accent-[#279682]" />
+            <span class="ml-3" :class="{ 'line-through': item.checked }">{{
+              item.message  
+            }}</span>
+          </div>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+  <teleport to="body">
+    <div v-if="open">
+      <Modal @close="open = false" @responceText="addtoList" />
+    </div>
+  </teleport>
 </template>
+
+<script setup>
+import { reactive, ref } from "vue";
+import Modal from "./components/Modal.vue";
+
+const open = ref(false);
+const myList = reactive([]);
+
+const addtoList = (item) => {
+  myList.push({ id: Date.now(), message: item, checked: false });
+  open.value = false;
+};
+</script>
---
+++ src/components/Modal.vue
@@ -0,0 +1,48 @@
+<template>
+  <div
+    class="modal bg-black/20 fixed top-0 w-full h-full flex justify-center items-center"
+  >
+    <div
+      class="modal bg-white shadow-xl rounded-xl w-3/5 h-[400px] p-8 items-center justify-center"
+      role="dialog"
+      ref="modal"
+    >
+      <div class="relative flex justify-between items-center">
+        <h1 class="font-medium text-2xl py-2">Add Item</h1>
+        <button
+          class="font-medium text-4xl absolute right-0 top-0 -mt-5 -mr-2 hover:text-red-400"
+          @click="$emit('close')"
+        >
+          ×
+        </button>
+      </div>
+      <div class="py-5">
+        <textarea
+          v-model="typingText"
+          name="txtArea"
+          id=""
+          rows="5"
+          placeholder="write here"
+          class="w-full resize-none outline-none border rounded-xl text-xl p-4"
+        ></textarea>
+      </div>
+      <div
+        class="flex justify-end hover:drop-shadow-[0_12px_9px_rgba(39,150,130,0.18)] pt-3"
+      >
+        <button
+          class="bg-[#f5f9f9] text-gray-700 h-12 px-3 rounded-2xl text-lg font-medium"
+          @click="$emit('responceText', typingText)"
+        >
+          Add
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "@vue/reactivity";
+
+defineEmits(['close','responceText']);
+const typingText = ref('');
+</script>
